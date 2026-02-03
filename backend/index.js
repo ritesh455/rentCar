c9e35b18407678b9429211446193a3766a55ae61
Om Ghode <ghodeom30@gmail.com>
2026-02-04 00:05:40 +0530

    cokkies change with added orgin and credentials in index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,10 @@ const cors=require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors(({
+  origin: "http://localhost:5173",
+  credentials: true
+})));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/health", async (req, res) => {
